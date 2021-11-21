d9af9b21915a6a222e6303c4c2ccfd7224ec5ed2
Szymon Skarzyński <01158894@pw.edu.pl>
2021-11-22 00:38:10 +0100

    Creation of Ticket class

---
+++ src/ticket/Ticket.java
@@ -0,0 +1,77 @@
+package ticket;
+
+import javax.lang.model.type.NullType;
+
+public class Ticket {
+    /*
+    Class that represents a ticket.
+    */
+
+    public String id;
+    public int price;
+    public String sector;
+    public int seat;
+    public Boolean isBought;
+    public String owner;
+
+    public Ticket(
+        String id,
+        int price,
+        String sector,
+        int seat
+    ) {
+        this.id = id;
+        this.price = price;
+        this.sector = sector;
+        this.seat = seat;
+        this.isBought = false;
+        this.owner = null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public float getPriceInPLN() {
+        return price / 100f;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public Boolean isBought() {
+        return isBought;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void removeOwner() {
+        this.owner = null;
+    }
+
+    public String toString() {
+        String ticketInfo = "ID: " + id;
+        ticketInfo += "\nPrice: " + String.format("%.2f", this.getPriceInPLN()) + " PLN";
+        ticketInfo += "\nSector " + sector;
+        ticketInfo += "\nSeat: " + seat;
+        ticketInfo += "\nIs bouth: " + isBought;
+        ticketInfo += "\nOwner: " + owner;
+
+        return ticketInfo;
+    }
+}
