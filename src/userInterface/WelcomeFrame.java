c24657f35718c18802a117db1d17a0274e9aa0d4
Marcin Grabysz <01158697@pw.edu.pl>
2021-11-22 14:00:10 +0100

    Add WelcomeFrame()

---
+++ src/userInterface/WelcomeFrame.java
@@ -0,0 +1,79 @@
+package userInterface;
+
+// ============================================================
+// Aby wywołać okienko, należy utworzyć obiekt klasy WelcomeFrame, na przykład:
+// new WelcomeFrame()
+// Na razie funkcja nic nie zwraca, bo jeszcze nie wiem jak
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class WelcomeFrame extends JFrame implements ActionListener {
+
+
+    JButton logButton, registerButton;
+    JLabel titleLabel;
+    Border border;
+
+    WelcomeFrame() {
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setSize(new Dimension(300, 450));
+
+        border = BorderFactory.createLineBorder(new Color(0x004169E1), 3);
+
+        titleLabel = new JLabel();
+        titleLabel.setText("EasyVENTER");
+        titleLabel.setFont(new Font("MV Boli", Font.PLAIN, 22));
+        titleLabel.setBounds(50, 50, 200, 50);
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        titleLabel.setBorder(border);
+        titleLabel.setBackground(Color.lightGray);
+        titleLabel.setOpaque(true);
+
+
+        logButton = new JButton();
+        logButton.setText("Log in");
+        logButton.setFocusable(false);
+        logButton.setBounds(50, 200, 200, 50);
+        logButton.addActionListener(this);
+
+        registerButton = new JButton();
+        registerButton.setText("Register (new user)");
+        registerButton.setFocusable(false);
+        registerButton.setBounds(50, 300, 200, 50);
+        registerButton.addActionListener(this);
+
+        this.add(titleLabel);
+        this.add(logButton);
+        this.add(registerButton);
+        this.setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource()==registerButton){
+            System.out.println("User wants to register");
+        } else if (e.getSource()==logButton){
+            System.out.println("User wants to log in");
+        }
+
+    }
+
+}
+
