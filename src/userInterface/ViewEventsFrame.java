92d41b411a56aa3bd37fe5d5da85fe2f8a318d5d
Szymon Skarzyński <01158894@pw.edu.pl>
2021-11-29 21:02:48 +0100

    Add ViewEventsFrame class

---
+++ src/userInterface/ViewEventsFrame.java
@@ -0,0 +1,87 @@
+package userInterface;
+
+import database.StringConstant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.ButtonGroup;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+
+// import java.awt.*;
+// import javax.swing.*;
+
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
+public class ViewEventsFrame extends JFrame implements ActionListener {
+
+    Border border;
+    JButton detailsButton, cancelButton;
+    ButtonGroup group;
+    
+    public ViewEventsFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setSize(new Dimension(1000, 600));
+
+        border = BorderFactory.createLineBorder(new Color(0x004169E1), 3);
+    
+        String[][] data = {
+            { "Meczyk jakiś", "Firma Krzak", "Bydgoszcz", "35.19.2022 25:72" },
+            { "Ludzie biegający w kółko", "Google", "Warszafka", "48.17.2023 29:81" }
+        };
+        String[] columnNames = {"Name", "Organizer", "Location", "Date and time"};
+ 
+        JTable table = new JTable(data, columnNames);
+        // table.setBounds(30, 30, 500, 300);
+        // this.add(table);
+
+        JScrollPane sp = new JScrollPane(table);
+        sp.setBounds(30, 30, 800, 500);
+        this.add(sp);
+
+
+        detailsButton = new JButton();
+        detailsButton.setText("Details");
+        detailsButton.setFocusable(false);
+        detailsButton.setBounds(850, 200, 120, 50);
+        detailsButton.addActionListener(this);
+        this.add(detailsButton);
+
+        cancelButton = new JButton();
+        cancelButton.setText("Cancel");
+        cancelButton.setFocusable(false);
+        cancelButton.setBounds(850, 300, 120, 50);
+        cancelButton.addActionListener(this);
+        this.add(cancelButton);
+
+        group = new ButtonGroup();
+        group.add(detailsButton);
+        group.add(cancelButton);
+        
+        this.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==detailsButton){
+            StringConstant.FRAME_TYPE = "create";
+        } else if (e.getSource()==cancelButton) {
+            StringConstant.FRAME_TYPE = "cancel";
+        }
+
+    }
+}
