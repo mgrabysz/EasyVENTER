13bac50554f1837dc12744ae864215c22e1519a3
Łukasz Szarejko <mslukaszsz@wp.pl>
2022-01-01 22:28:16 +0100

    getters for mainmenu

--- pap/src/main/java/kowale/userInterface/MainMenuFrame.java
+++ pap/src/main/java/kowale/userInterface/MainMenuFrame.java
@@ -12,6 +12,11 @@ public class MainMenuFrame extends JFrame implements ActionListener{
 
     JButton seeEvents, makeEvent, logout, manageEvent, manageTicket;
     private String user_type;
+    private boolean see = false;
+    private boolean make = false;
+    private boolean manageE = false;
+    private boolean manageT = false;
+    private boolean cancel = false;
     public int decision = 0;
 
     public MainMenuFrame(String type){
@@ -50,24 +55,44 @@ public class MainMenuFrame extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e){
         if (e.getSource()==logout){
-
+            cancel = true;
             this.dispose();
         }
         else if (e.getSource()==seeEvents){
-
+            see = true;
             this.dispose();
         }
         else if (e.getSource()==makeEvent){
-
+            make = true;
             this.dispose();
         }
         else if (e.getSource()==manageEvent){
-
+            manageE = true;
             this.dispose();
         }
         else if (e.getSource()==manageTicket){
-
+            manageT = true;
             this.dispose();
         }
+    }
+
+    public boolean getCancel(){
+        return cancel;
+    }
+
+    public boolean getSee(){
+        return see;
+    }
+
+    public boolean getMake(){
+        return make;
+    }
+
+    public boolean getManageT(){
+        return manageT;
+    }
+
+    public boolean getManageE(){
+        return manageE;
     }
 }
