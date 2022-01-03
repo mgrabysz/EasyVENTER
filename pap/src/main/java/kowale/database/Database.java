c6f201d69239614298917c872b101167dcab8047
Łukasz Szarejko <mslukaszsz@wp.pl>
2022-01-03 12:52:19 +0100

    jdbc first use

--- pap/src/main/java/kowale/database/Database.java
+++ pap/src/main/java/kowale/database/Database.java
@@ -2,10 +2,18 @@ package kowale.database;
 import kowale.user.User;
 import kowale.event.Event;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 // import java.time.LocalDateTime;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Database {
     /*
@@ -14,7 +22,7 @@ public class Database {
 
     public HashMap<String, NewUserData> usersData = new HashMap<String, NewUserData>();
     public ArrayList<User> users = new ArrayList<>();
-
+    public Connection connection = null;
     public ArrayList<Event> events = new ArrayList<>();
 
     public Database(){ // Constructor
@@ -60,4 +68,76 @@ public class Database {
     public ArrayList<Event> getEvents() {
         return events;
     }
+
+    public void connectDatabase() throws SQLException {
+        String password = "";
+        // TODO
+        try {
+            connection = DriverManager.getConnection("ora4.ii.pw.edu.pl", "z01", password);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void closeDatabase() throws SQLException {
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public boolean registerUser(User user) throws SQLException{
+        if (connection == null){
+            return false;
+        }
+        boolean success;
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = connection.prepareStatement("INSERT INTO users VALUES (?, ?, ?, ?)");
+            pstmt.setString(0, user.getName());
+// TODO
+            success = (pstmt.executeUpdate() == 1);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            return false;
+        } finally {
+            try {
+                pstmt.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+                return false;
+            }
+        }
+        return success;
+    }
+
+    public LinkedList<User> getAllUsersCredentials(){
+        LinkedList<User> users = new LinkedList<User>();
+        Statement stmt = null;
+        if (connection != null) {
+            try {
+                stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM users_credentials");
+                while (rs.next()) {
+                    // TODO
+                    User user;
+                    // TODO
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            } finally {
+                if (stmt != null){
+                    try {
+                        stmt.close();
+                    } catch (SQLException ex) {
+                        System.out.println(ex);
+                    }
+                }
+            }
+        }
+        return users;
+    }
+
 }
