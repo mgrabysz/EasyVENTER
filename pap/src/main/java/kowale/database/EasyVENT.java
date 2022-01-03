6ac03d019bc9d293b7c6d3bf54969f50bfe3756b
Szymon Skarzyński <01158894@pw.edu.pl>
2022-01-03 19:22:53 +0100

    remove unused code

--- pap/src/main/java/kowale/database/EasyVENT.java
+++ pap/src/main/java/kowale/database/EasyVENT.java
@@ -68,18 +68,6 @@ public class EasyVENT {
         return hexString.toString();
     }
 
-    private static String bytesToString(byte[] hash) {
-        StringBuilder hexString = new StringBuilder(2 * hash.length);
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if(hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
-
     public void mainLoop() throws NoSuchAlgorithmException {
         boolean runLoop = true;
         GlobalVariables.FRAME_TYPE = "Welcome";
