c5c3352a818540aa15c16f27501af73962ecf7eb
Łukasz Szarejko <mslukaszsz@wp.pl>
2022-01-03 21:48:21 +0100

    fix tests

--- pap/src/test/java/kowale/EventOrganizerTest.java
+++ pap/src/test/java/kowale/EventOrganizerTest.java
@@ -9,7 +9,8 @@ import java.util.LinkedList;
 import org.junit.Test;
 
 public class EventOrganizerTest {
-    EventOrganizer eo = new EventOrganizer("John", "Rambo", "johnny", "1234");
+    EventOrganizer eo = new EventOrganizer("John", "Rambo", "johnny", "1234",
+    "email@wp.pl", 666666666, "krzak firma");
     LocalDateTime dt;
     int numOfSectors;
     // HashMap<String, Integer> tickets;
--- pap/src/test/java/kowale/UserTest.java
+++ pap/src/test/java/kowale/UserTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class UserTest{
-    User user = new User("John", "Rambo", "johnny", "1234");
+    User user = new User("John", "Rambo", "johnny", "1234", "email@gmail.com", 666666666);
 
     @Test
     public void getLoginTest(){
