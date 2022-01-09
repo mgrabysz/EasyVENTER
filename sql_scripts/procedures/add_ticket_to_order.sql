331728739c44cff527c0d01175b392629ab1c228
pbedkowski <01158591@pw.edu.pl>
2022-01-09 19:07:35 +0100

    created proc to add ticket to specific order

---
+++ sql_scripts/procedures/add_ticket_to_order.sql
@@ -0,0 +1,20 @@
+/*
+
+    + ================= +
+    |   Created by PB   |
+    + ================= +
+
+*/
+
+CREATE OR REPLACE PROCEDURE add_ticket_to_order(
+t_ticket_id NUMERIC,
+o_order_id NUMERIC,
+c_category_id NUMERIC
+)
+AS
+client_id_exists NUMBER;
+BEGIN
+    INSERT INTO TICKET_ORDERS (TICKET_ID, ORDER_ID, CATEGORY_ID)
+    VALUES (t_ticket_id, o_order_id, c_category_id);
+END add_ticket_to_order;
+    
