c06fe285839ee8050b0ad9ad096a75237917a2d4
pbedkowski <01158591@pw.edu.pl>
2022-01-10 17:52:19 +0100

    removed category from add ticket procedure

--- sql_scripts/procedures/add_ticket.sql
+++ sql_scripts/procedures/add_ticket.sql
@@ -8,7 +8,6 @@
 
 CREATE OR REPLACE PROCEDURE add_ticket(
 e_event_id NUMBER,
-e_category VARCHAR,
 e_seat VARCHAR,
 e_sector VARCHAR,
 e_price VARCHAR
@@ -21,30 +20,6 @@ BEGIN
     -- CHECK IF EVENT EXISTS
     SELECT COUNT(1) INTO event_exists
     FROM EVENTS WHERE EVENT_ID = e_event_id;
-    
-    -- INSERT CATEGORY IF NOT EXIST
-    SELECT COUNT(1) INTO category_exists
-    FROM TICKET_CATEGORIES WHERE CATEGORY_NAME = e_category;
-    
-    -- COMPARE CATEGORIES
-    -- ONLY 3 are accepted
-    IF event_exists > 0 THEN
-        if e_category = 'VIP' THEN
-            discount_value:=1.5;
-        elsif e_category = 'CHILD' THEN
-            discount_value:=0.7;
-        elsif e_category = 'ADULT' THEN
-            discount_value:=1;
-        END IF;
-        -- INSERT TICKET CATEGORY
-        if category_exists < 0 THEN
-            INSERT INTO TICKET_CATEGORIES(CATEGORY_NAME, DISCOUNT)
-            VALUES (e_category, discount_value);
-        END IF;
-    ELSE
-        RAISE NO_DATA_FOUND;  -- EVENT was not found
-    END IF;
-    
 
     -- INSERT TICKET
     IF event_exists > 0 THEN
