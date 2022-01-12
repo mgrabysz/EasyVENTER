1f0a30842b5295e4b8f7424996791d7cf6ccffa8
Łukasz Szarejko <mslukaszsz@wp.pl>
2022-01-12 19:22:58 +0100

    fix bug

--- sql_scripts/procedures/register_client.sql
+++ sql_scripts/procedures/register_client.sql
@@ -2,7 +2,7 @@ create or replace PROCEDURE register_client (
     login VARCHAR2,
     password VARCHAR2,
     name VARCHAR2,
-    surname VARCHAR2,
+    csurname VARCHAR2,
     email VARCHAR2,
     birth DATE,
     gen CHAR,
@@ -11,8 +11,8 @@ create or replace PROCEDURE register_client (
 IS
     nuser_id NUMBER;
 BEGIN
-    INSERT INTO z01.client_data (name, surname, email, birth_date, gender, telephone) VALUES (name, surname, email, birth, gen, telephone);
+    INSERT INTO z01.client_data (name, surname, email, birth_date, gender, telephone) VALUES (name, csurname, email, birth, gen, telephone);
 
-    SELECT client_id INTO nuser_id FROM z01.client_data WHERE z01.client_data.surname = surname ORDER BY client_id DESC NULLS LAST FETCH FIRST 1 ROWS ONLY;
+    SELECT client_id INTO nuser_id FROM z01.client_data WHERE z01.client_data.surname = csurname ORDER BY client_id DESC NULLS LAST FETCH FIRST 1 ROWS ONLY;
     INSERT INTO z01.user_credentials (user_id, account_type, login, password) VALUES (nuser_id, 'C', login, password);
 END;
--- sql_scripts/procedures/register_organizer.sql
+++ sql_scripts/procedures/register_organizer.sql
@@ -2,7 +2,7 @@ create or replace PROCEDURE register_organizer (
     login VARCHAR2,
     password VARCHAR2,
     name VARCHAR2,
-    surname VARCHAR2,
+    osurname VARCHAR2,
     email VARCHAR2,
     telephone NUMBER,
     company_name VARCHAR
@@ -19,8 +19,8 @@ BEGIN
     END IF;
     SELECT company_id INTO company FROM z01.companies WHERE company_name = z01.companies.name;
     SELECT SYSDATE INTO date_reg FROM dual;
-    INSERT INTO z01.organizer_data(name, surname, email, telephone, date_registered, company_id) VALUES (name, surname, email, telephone, date_reg, company);
+    INSERT INTO z01.organizer_data(name, surname, email, telephone, date_registered, company_id) VALUES (name, osurname, email, telephone, date_reg, company);
 
-    SELECT organizer_id INTO user_id FROM z01.organizer_data WHERE z01.organizer_data.surname = surname ORDER BY organizer_id DESC NULLS LAST FETCH FIRST 1 ROWS ONLY;
+    SELECT organizer_id INTO user_id FROM z01.organizer_data WHERE z01.organizer_data.surname = osurname ORDER BY organizer_id DESC NULLS LAST FETCH FIRST 1 ROWS ONLY;
     INSERT INTO z01.user_credentials (user_id, account_type, login, password) VALUES (user_id, 'O', login, password);
 END;
