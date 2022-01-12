748022b6a26e56ca4e2849a3e580159eab66dda7
pbedkowski <01158591@pw.edu.pl>
2022-01-12 03:21:35 +0100

    BIG error resolved of editing event

--- sql_scripts/procedures/edit_event.sql
+++ sql_scripts/procedures/edit_event.sql
@@ -3,16 +3,14 @@ e_name VARCHAR2,
 e_country VARCHAR2,
 e_city VARCHAR2,
 e_street VARCHAR2,
-e_date_time DATE)
+e_date_time TIMESTAMP)
 AS
 e_country_id NUMBER;
-
 e_city_id NUMBER;
-city_exists NUMBER;
-
+country_exists NUMBER;
+city_and_country_exist NUMBER;
 e_address_id NUMBER;
 street_exists NUMBER;
-
 event_exists NUMBER;
 e_event_id NUMBER;
 e_detail_id NUMBER;
@@ -20,48 +18,66 @@ BEGIN
     -- CHECK IF EVENT TO MODIFY EXISTS
     SELECT 1, EVENT_ID INTO event_exists, e_event_id
     FROM EVENTS WHERE EVENT_NAME = e_name;
+    
+    -- set old address id
+    SELECT ADDRESS_ID INTO e_address_id
+    FROM EVENT_DETAILS
+    WHERE EVENT_ID = e_event_id;
 
     IF event_exists > 0 THEN
+        
+        -- CHECK IF country exists
+        SELECT COUNT(1) INTO country_exists
+        FROM COUNTRIES
+        WHERE COUNTRY_NAME = e_country;
+        
+        IF country_exists <= 0 THEN
+            INSERT INTO COUNTRIES(COUNTRY_NAME)
+            VALUES(e_country)
+            RETURNING COUNTRY_ID INTO e_country_id;
+        ELSE
+            SELECT COUNTRY_ID INTO e_country_id
+            FROM COUNTRIES
+            WHERE COUNTRY_NAME = e_country;
+        END IF;
+--        FROM EVENTS WHERE EVENT_NAME = e_name;
         -- insert country if it doesn't yet exist in table
-        INSERT INTO COUNTRIES(COUNTRY_NAME) 
-        SELECT e_country
-        FROM DUAL
-        WHERE NOT EXISTS (SELECT * FROM COUNTRIES WHERE COUNTRY_NAME = e_country);
-
-        SELECT COUNTRY_ID INTO e_country_id FROM COUNTRIES WHERE COUNTRY_NAME = e_country;
+--        INSERT INTO COUNTRIES(COUNTRY_NAME) 
+--        SELECT e_country
+--        FROM DUAL
+--        WHERE NOT EXISTS (SELECT * FROM COUNTRIES WHERE COUNTRY_NAME = e_country);
+--
+--        SELECT COUNTRY_ID INTO e_country_id FROM COUNTRIES WHERE COUNTRY_NAME = e_country;
 
         -- Check if city name exists
-        SELECT 1 INTO city_exists
-        FROM CITIES WHERE NAME = e_city;
+        SELECT COUNT(1) INTO city_and_country_exist
+        FROM CITIES
+        WHERE NAME = e_city AND COUNTRY_ID = e_country_id;
 
-        IF city_exists > 0 THEN
+        IF city_and_country_exist > 0 THEN
+            SELECT CITY_ID INTO e_city_id
+            FROM CITIES
+            WHERE NAME = e_city AND COUNTRY_ID = e_country_id;
             -- city exists, update country for that city
-            UPDATE CITIES SET COUNTRY_ID = e_country_id;
+--            UPDATE CITIES SET COUNTRY_ID = e_country_id;
+--            INSERT INTO CITIES(NAME, COUNTRY_ID) 
+--            SELECT e_city, e_country_id
+--            FROM DUAL
+--            WHERE NOT EXISTS (SELECT * FROM CITIES WHERE NAME = e_city
+--            AND COUNTRY_ID=e_country_id);
         ELSE
             INSERT INTO CITIES(NAME, COUNTRY_ID)
-            SELECT e_city, e_country_id
-            FROM DUAL
-            WHERE NOT EXISTS (SELECT * FROM CITIES WHERE NAME=e_city);
+            VALUES(e_city, e_country_id)
+            RETURNING CITY_ID INTO e_city_id;
         END IF;
-
-        SELECT CITY_ID INTO e_city_id FROM CITIES WHERE NAME=e_city;
-
-        -- Check if street exists
-        SELECT 1 INTO street_exists
-        FROM ADDRESSES WHERE STREET = e_street;
-
-        IF street_exists > 0 THEN
-            -- street exists, update city for that street
-            UPDATE ADDRESSES SET CITY_ID = e_city_id;
-        ELSE
-            INSERT INTO ADDRESSES(STREET, CITY_ID)
-            SELECT e_street, e_city_id
-            FROM DUAL;
-        END IF;
-
-        SELECT ADDRESS_ID INTO e_address_id FROM ADDRESSES
-        WHERE STREET=e_street;
-
+        
+        UPDATE ADDRESSES SET
+        CITY_ID = e_city_id,
+        STREET = e_street
+        WHERE ADDRESS_ID = e_address_id;
+        
+        -- now e_address_id holds new address with updated city and country
+        
         -- COUNTRY, CITY, ADDRESS have been inserted --
 
         -- UPDATE DATA TO EVENT DETAILS --
